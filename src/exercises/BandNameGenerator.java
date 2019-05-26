8f982f3804a333892e6bbf943c69d88e75c15c30
Your Name <you@example.com>
2019-05-26 17:31:38 +0200

    BandNameGenerator - generating name which constists of random elements of two ArrayLists

--- src/exercises/BandNameGenerator.java
+++ src/exercises/BandNameGenerator.java
@@ -1,5 +1,8 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /* 
  * Make 2 ArrayLists; 
  *      1 full of adjectives (e.g. slimy, blind)
@@ -8,5 +11,35 @@ package exercises;
  * When the program is run, one word is randomly selected from each list to generate a band name.
 */
 public class BandNameGenerator {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> adjectives = new ArrayList<String>();
+		adjectives.add("Wonderful");
+		adjectives.add("Mad");
+		adjectives.add("Lost");
+		adjectives.add("Silent");
+		adjectives.add("Majestic");
+		adjectives.add("Wild");
+		adjectives.add("Hypnotic");
+		System.out.println(adjectives);
+
+		ArrayList<String> nouns = new ArrayList<String>();
+		nouns.add("Men");
+		nouns.add("Angels");
+		nouns.add("Devils");
+		nouns.add("Voices");
+		nouns.add("Rebels");
+		nouns.add("Things");
+		nouns.add("Whisperers");
+		System.out.println(nouns);
+
+		for (int i = 0; i < 20; i++) {
+			int random1 = new Random().nextInt(adjectives.size());
+			int random2 = new Random().nextInt(nouns.size());
+			System.out.println(adjectives.get(random1) + " " + nouns.get(random2));
+		}
+
+	}
 
 }
